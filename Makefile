3c4f9cc7dcb11c137cc52af9e9915129bd3565ad
Ahmed Fasih <wuzzyview@gmail.com>
2020-11-02 20:58:02 -0500

    Update to Nov 2020 data, with fix for Yahoo null data

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ browser.bundle.js: browser.js shiller.js
 	npm run bundle
 watch:
 	fswatch -0 -o -l .1 browser.js shiller.js | xargs -0 -n 1 -I {} make
+data/ie_data.xls:
+	cd data && wget http://www.econ.yale.edu/~shiller/data/ie_data.xls && cd ..
+data/ie_data.xls.json: data/ie_data.xls.json
+	npm run build && node node.js
--- yahoo-finance.ts
+++ yahoo-finance.ts
@@ -5,21 +5,23 @@ var dsv = require('d3-dsv');
 const columnsExpected = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'];
 
 export function parseRawCSV(raw: string): MonthlyData[] {
-  let csv = dsv.csvParse(raw);
+  let csv: any[]&{columns: any} = dsv.csvParse(raw);
   if (csv.columns.join(',') !== columnsExpected.join(',')) { throw new Error('unexpected columns'); }
   let aoa: MonthlyData[] = csv.map((o: any) => {
-    const d = new Date(o.Date);
-    let md: MonthlyData = {
-      year: d.getUTCFullYear(),
-      month: d.getUTCMonth() + 1,
-      price: o['Adj Close'],
-      div: 0,
-      cpi: 1,
-      interest10y: 0,
-      realDiv: 0,
-      realPrice: o['Adj Close']
-    };
-    return md;
-  });
+                                if (!o['Adj Close'] || o['Adj Close'] === 'null') { return null; }
+                                const d = new Date(o.Date);
+                                let md: MonthlyData = {
+                                  year: d.getUTCFullYear(),
+                                  month: d.getUTCMonth() + 1,
+                                  price: o['Adj Close'],
+                                  div: 0,
+                                  cpi: 1,
+                                  interest10y: 0,
+                                  realDiv: 0,
+                                  realPrice: o['Adj Close']
+                                };
+                                return md;
+                              })
+                               .filter(x => !!x) as MonthlyData[];
   return aoa;
 }
